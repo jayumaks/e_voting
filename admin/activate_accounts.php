91b87108e193860c7bde16981a93fa35bb0e9ef6
jayumaks <jayumaks@gmail.com>
2025-06-09 22:00:15 +0100

    use id number

--- admin/activate_accounts.php
+++ admin/activate_accounts.php
@@ -2,9 +2,9 @@
 require_once 'dbcon.php';
 
 if (isset($_GET['id']) && !empty($_GET['id'])) {
-    $student_id = $conn->real_escape_string($_GET['id']); // Sanitize input
+    $id_number = $conn->real_escape_string($_GET['id']); // Sanitize input
 
-    $sql = "UPDATE voters SET status = 'Active' WHERE id = '$student_id'";
+    $sql = "UPDATE voters SET status = 'Active' WHERE id = '$id_number'";
     $update = $conn->query($sql);
 
     if ($update) {
