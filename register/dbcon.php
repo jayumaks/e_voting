f083e106022b95cb240bd0601f9afb9bbea2bb25
jayumaks <jayumaks@gmail.com>
2025-06-09 14:22:16 +0100

    added db config

--- register/dbcon.php
+++ register/dbcon.php
@@ -1,5 +1,6 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'vote');
+	// $conn = new mysqli('localhost', 'root', '', 'vote');
+	$conn = new mysqli('localhost', 'aauekpo5_voting', '=W4(Q=fedyjG', 'aauekpo5_voting');
 	
 	if(!$conn){
 		die("Error: Failed to connect to database");
