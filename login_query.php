12f87aebc85e67366588ee5a36196d53478a15ff
jayumaks <jayumaks@gmail.com>
2025-06-10 06:26:34 +0100

    idno

--- login_query.php
+++ login_query.php
@@ -6,7 +6,7 @@
 		$idno=$_POST['idno'];
 		$password=$_POST['password'];
 
-		$result = $conn->query("SELECT * FROM voters WHERE id_number = '$idno' && password = '".md5($password)."' && `account` = 'active' && `status` = 'Unvoted'") or die(mysqli_errno());
+		$result = $conn->query("SELECT * FROM voters WHERE id_number = '$idno' && password = '".md5($password)."' && `account` = 'Active' && `status` = 'Unvoted'") or die(mysqli_errno());
 		$row = $result->fetch_array();
 		$voted = $conn->query("SELECT * FROM `voters` WHERE id_number = '$idno' && password = '".md5($password)."' && `status` = 'Voted'")->num_rows;
 		$numberOfRows = $result->num_rows;
@@ -27,7 +27,7 @@
 		}else{
 			?>
 			<script type="text/javascript">
-			alert('Your account is not Actived')
+			alert('Your account is not Activated')
 			</script>
 			<?php
 		}
