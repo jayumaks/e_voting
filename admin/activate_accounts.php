7b09935162ffc6b05f2aa9f05df4536d537d545c
jayumaks <jayumaks@gmail.com>
2025-06-11 11:46:29 +0100

    adjusted activate

--- admin/activate_accounts.php
+++ admin/activate_accounts.php
@@ -1,5 +1,5 @@
  <?php 
 	require_once 'dbcon.php';						
-	$conn->query("UPDATE voters SET status = 'Active' WHERE id = $student_id")or die($conn->error);
+	$conn->query("UPDATE voters SET status = 'Active'")or die($conn->error);
 	echo "<script> window.location='voters.php' </script>";
 ?>			 
