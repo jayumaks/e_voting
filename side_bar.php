da76352e2f29a17a2c394995f020bab4e0ed4312
jayumaks <jayumaks@gmail.com>
2025-06-17 17:04:41 +0100

    adjusted the sidebar.php

--- side_bar.php
+++ side_bar.php
@@ -1,29 +1,56 @@
-<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom:0; background-color:black;">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="index.php" style = "color:white; padding-left:25px;"><i class = "fa fa-home fa-large" > </i>HOME | AAU Online Voting System</a>
-				
-            </div>
-      
+<!-- sidebar.php -->
+<nav class="navbar navbar-inverse sidebar" role="navigation" style="background-color: black; border: none;">
+  <div class="container-fluid">
+    
+    <!-- Sidebar Header with Hamburger and Brand -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#sidebar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php" style="color: white; font-weight: bold;">
+        <i class="fa fa-home"></i> AAU Voting System
+      </a>
+    </div>
 
-            <ul class="nav navbar-top-links navbar-right">
-            
-               <?php require 'admin/dbcon.php';
-				$query = $conn->query("SELECT * from voters where voters_id ='$session_id'")or die (mysql_error ());
-				
-				while ($row = $query->fetch_array()){
-				
-				
-			 ?>
-               
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" style = "color: white">
-					<i>Welcome: <?php echo $user_username = $user_row['firstname']." ".$user_row['lastname'];?></i>
-                    </a>                     
-                </li>
-            <li class="dropdown">
-                <a href="logout.php"style = "color: white; padding-rignt: 30px;"> <i class = "fa fa-sign-out" style = "color: white"></i>Logout</a>
-            </li>
-           
-            </ul>
-			<?php }?>
-        </nav>
+    <!-- Collapsible Sidebar Content -->
+    <div class="collapse navbar-collapse" id="sidebar-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="dashboard.php" style="color: white;"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="polls.php" style="color: white;"><i class="fa fa-bar-chart"></i> View Polls</a></li>
+        <li><a href="vote.php" style="color: white;"><i class="fa fa-check-square-o"></i> Cast Vote</a></li>
+        <li><a href="results.php" style="color: white;"><i class="fa fa-line-chart"></i> Results</a></li>
+        <li><a href="logout.php" style="color: white;"><i class="fa fa-sign-out"></i> Logout</a></li>
+      </ul>
+    </div>
+
+  </div>
+</nav>
+
+<!-- Optional Sidebar Styling -->
+<style>
+  .sidebar {
+    min-height: 100vh;
+    padding-top: 20px;
+  }
+
+  .sidebar .nav > li > a {
+    padding: 12px 20px;
+    display: block;
+    font-size: 16px;
+    color: #ccc;
+  }
+
+  .sidebar .nav > li > a:hover {
+    background-color: #111;
+    color: #fff;
+  }
+
+  @media (max-width: 768px) {
+    .sidebar {
+      position: relative;
+      width: 100%;
+    }
+  }
+</style>
