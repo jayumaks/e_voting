aaa52ada599751ba1af98488d24b107db5bf3420
jayumaks <jayumaks@gmail.com>
2025-06-17 14:54:50 +0100

    added poll pages

---
+++ poll.php
@@ -0,0 +1,21 @@
+<?php include('head.php'); include('sess.php'); ?>
+<body>
+<?php include('side_bar.php'); ?>
+<div class="container">
+    <h3>Opinion Poll</h3>
+    <?php
+        require 'dbcon.php';
+        $poll = $conn->query("SELECT * FROM poll_question ORDER BY id DESC LIMIT 1")->fetch_assoc();
+        $options = $conn->query("SELECT * FROM poll_option WHERE poll_id = {$poll['id']}");
+    ?>
+    <form method="POST" action="poll_submit.php">
+        <h4><?php echo $poll['question']; ?></h4>
+        <?php while ($opt = $options->fetch_assoc()) { ?>
+            <input type="radio" name="option_id" value="<?php echo $opt['id']; ?>" required>
+            <?php echo $opt['option_text']; ?><br>
+        <?php } ?>
+        <button type="submit" class="btn btn-success">Submit</button>
+    </form>
+</div>
+<?php include('footer.php'); ?>
+</body>
---
+++ poll_submit.php
@@ -0,0 +1,11 @@
+<?php
+require 'dbcon.php';
+
+if (isset($_POST['option_id'])) {
+    $option_id = intval($_POST['option_id']);
+    $conn->query("UPDATE poll_option SET votes = votes + 1 WHERE id = $option_id");
+    header("Location: poll_result.php");
+} else {
+    echo "No option selected.";
+}
+?>
