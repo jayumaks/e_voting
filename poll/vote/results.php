0be46a1dcc53fec0df4c1c77bc81c70501e1bc1b
jayumaks <jayumaks@gmail.com>
2025-06-25 15:54:00 +0100

    poll config

--- poll/vote/results.php
+++ poll/vote/results.php
@@ -1,7 +1,9 @@
 <?php
 
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 
 include('../dbcon.php');
 $poll = $pdo->query("SELECT * FROM poll LIMIT 1")->fetch();
