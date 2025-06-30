01a49fde8016ab8bf476d742139858df0a87c9bc
jayumaks <jayumaks@gmail.com>
2025-06-30 09:54:58 +0100

    poll config

--- poll/admin/poll_report.php
+++ poll/admin/poll_report.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-include('dbcon.php');
+include('../dbcon.php');
 
 // Fetch the latest poll
 $poll = $pdo->query("SELECT * FROM poll ORDER BY id DESC LIMIT 1")->fetch();
