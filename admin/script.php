b2c3b92194d005cbc80b26537fd03cf311bebc82
jayumaks <jayumaks@gmail.com>
2025-07-21 01:35:21 +0100

    fix add candidate page

--- admin/script.php
+++ admin/script.php
@@ -1,43 +1,28 @@
-  <!-- jQuery Version 1.11.0 -->
-    <script src="../js/jquery-1.11.0.js"></script>
+<!-- jQuery Version 1.11.0 -->
+<script src="../js/jquery-1.11.0.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../js/bootstrap.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="../js/bootstrap.min.js"></script>
 
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../js/plugins/metisMenu/metisMenu.min.js"></script>
+<!-- Metis Menu Plugin JavaScript -->
+<script src="../js/plugins/metisMenu/metisMenu.min.js"></script>
 
-    <!-- Custom Theme JavaScript -->
-    <script src="../js/sb-admin-2.js"></script>
+<!-- DataTables JavaScript -->
+<script src="../js/plugins/dataTables/jquery.dataTables.js"></script>
+<script src="../js/plugins/dataTables/dataTables.bootstrap.js"></script>
 
+<!-- Custom Theme JavaScript -->
+<script src="../js/sb-admin-2.js"></script>
 
-
-	<!--DAta-table-->
-
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../js/plugins/metisMenu/metisMenu.min.js"></script>
-
-    <!-- DataTables JavaScript -->
-    <script src="../js/plugins/dataTables/jquery.dataTables.js"></script>
-    <script src="../js/plugins/dataTables/dataTables.bootstrap.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../js/sb-admin-2.js"></script>
-
-    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
-    <script>
-    $(document).ready(function () {
-        $('#dataTables-example').DataTable({
-            lengthMenu: [
-                [10, 25, 50, 100, 500, 1000, -1],
-                [10, 25, 50, 100, 500, 1000, "All"]
-            ],
-            pageLength: -1 // Show all records
-        });
+<!-- DataTables Init -->
+<script>
+$(document).ready(function () {
+    $('#dataTables-example').DataTable({
+        lengthMenu: [
+            [10, 25, 50, 100, 500, 1000, -1],
+            [10, 25, 50, 100, 500, 1000, "All"]
+        ],
+        pageLength: -1 // Show all records
     });
+});
 </script>
-
