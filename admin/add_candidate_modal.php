d72df36a66e0d5047b5a121ba2f6db521778882a
jayumaks <jayumaks@gmail.com>
2025-07-21 01:04:17 +0100

    fix add candidate page

--- admin/add_candidate_modal.php
+++ admin/add_candidate_modal.php
@@ -1,120 +1,112 @@
+<?php
+// Top of the file (before any HTML)
+session_start();
+require_once 'dbcon.php';
+
+if (isset($_POST['save'])) {
+    $party = $_POST['party'];
+    $position = $_POST['position'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $year_level = $_POST['year_level'];
+    $gender = $_POST['gender'];
+
+    // Handle image upload
+    $image = $_FILES['image'];
+    $image_name = addslashes($image['name']);
+    $tmp_name = $image['tmp_name'];
+    $location = "upload/" . $image_name;
+
+    if (move_uploaded_file($tmp_name, $location)) {
+        $stmt = $conn->prepare("INSERT INTO candidate(position, party, firstname, lastname, year_level, gender, img) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssssss", $position, $party, $firstname, $lastname, $year_level, $gender, $location);
+
+        if ($stmt->execute()) {
+            $_SESSION['success'] = "Candidate added successfully.";
+        } else {
+            $_SESSION['error'] = "Failed to add candidate: " . $stmt->error;
+        }
+        $stmt->close();
+    } else {
+        $_SESSION['error'] = "Image upload failed.";
+    }
+
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+?>
+
+
+<!-- Add this modal HTML inside your page -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
         <div class="modal-content">
 			<div class="modal-header">
-				<h4 class="modal-title" id="myModalLabel">         
-					<div class="panel panel-primary">
-						<div class="panel-heading">
-							<center>Add Candidate</center>
-						</div>    
-					</div>
-				</h4>
+				<h4 class="modal-title" id="myModalLabel">Add Candidate</h4>
 			</div>
-										
-										
+
             <div class="modal-body">
-				<form method = "post" enctype = "multipart/form-data">	
+				<form method="post" enctype="multipart/form-data">
 					<div class="form-group">
 						<label>Position</label>
-						<select class = "form-control" name = "position">
-								<option selected disabled>Select Candidate Group</option>
-								
-								<option>President</option>
-								<option>Vice President</option>
-								
-								
-								
-								<option>Treasurer</option>
-								<option>Secretary General</option>
-								
-								<option>Welfare</option>
-								<option>PRO</option>
-								
-							</select>
+						<select class="form-control" name="position" required>
+							<option selected disabled>Select Candidate Group</option>
+							<option>President</option>
+							<option>Vice President</option>
+							<option>Treasurer</option>
+							<option>Secretary General</option>
+							<option>Welfare</option>
+							<option>PRO</option>
+						</select>
 					</div>
-					
+
 					<div class="form-group">
 						<label>Party Name</label>
-							<input class="form-control" type ="text" name = "party" placeholder="Please enter party name" required="true">
+						<input class="form-control" type="text" name="party" placeholder="Enter party name" required>
 					</div>
 
-										
 					<div class="form-group">
 						<label>Firstname</label>
-							<input class="form-control" type ="text" name = "firstname" placeholder="Please enter firstname" required="true">
+						<input class="form-control" type="text" name="firstname" placeholder="Enter firstname" required>
 					</div>
+
 					<div class="form-group">
 						<label>Lastname</label>
-							<input class="form-control"  type = "text" name = "lastname" placeholder="Please enter lastname" required="true">
+						<input class="form-control" type="text" name="lastname" placeholder="Enter lastname" required>
 					</div>
-											
+
 					<div class="form-group">
-						<label>Year_Level</label>
-							<select class = "form-control" name = "year_level">
-								<option></option>
-								<option>1st Year</option>
-								<option>2nd Year</option>
-								<option>3rd Year</option>
-								<option>4th Year</option>
-							</select>
+						<label>Year Level</label>
+						<select class="form-control" name="year_level" required>
+							<option selected disabled>Select Level</option>
+							<option>1st Year</option>
+							<option>2nd Year</option>
+							<option>3rd Year</option>
+							<option>4th Year</option>
+						</select>
 					</div>
-															
+
 					<div class="form-group">
 						<label>Gender</label>
-							<select class = "form-control" name = "gender">
-								<option></option>
-								<option>Male</option>
-								<option>Female</option>
-							</select>
+						<select class="form-control" name="gender" required>
+							<option selected disabled>Select Gender</option>
+							<option>Male</option>
+							<option>Female</option>
+						</select>
 					</div>
-											
-											
-					<div class="form-group">
-                        <label>Image</label>
-						<input type="file" name="image"required> 
-                    </div>
-						<button name = "save" type="submit" class="btn btn-primary">Save Data</button>
-				</form>  
-			</div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-										
-										
-										
-										
-			<?php 
-				require_once 'dbcon.php';
-				
-				if (isset ($_POST ['save'])){
-					
-					
-					$party=$_POST['party'];
-					$position=$_POST['position'];					
-					$party=$_POST['party'];
-					$firstname=$_POST['firstname'];
-					$lastname=$_POST['lastname'];
-					$year_level=$_POST['year_level'];
-					$gender=$_POST['gender'];
-					$image= addslashes(file_get_contents($_FILES['image']['tmp_name']));
-					$image_name= addslashes($_FILES['image']['name']);
-					$image_size= getimagesize($_FILES['image']['tmp_name']);
-		
-					move_uploaded_file($_FILES["image"]["tmp_name"],"upload/" . $_FILES["image"]["name"]);			
-					$location="upload/" . $_FILES["image"]["name"];
-					
-					
-					 $conn->query("INSERT INTO candidate(position,party,firstname,lastname,year_level,gender,img)values('$position','$party','$firstname','$lastname','$year_level','$gender','$location')")or die($conn->error);
-					
-				}
 
-			?>					
+					<div class="form-group">
+						<label>Image</label>
+						<input type="file" name="image" class="form-control" required>
+					</div>
+
+					<button name="save" type="submit" class="btn btn-primary">Save Candidate</button>
+				</form>
+			</div>
+
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
         </div>
-                                   
-<!-- /.modal-content -->
-                                
 	</div>
-                               
-<!-- /.modal-dialog -->
-								
 </div>
--- admin/candidate.php
+++ admin/candidate.php
@@ -1,115 +1,132 @@
-<?php include ('session.php');?>
-<?php include ('head.php');?>
+<?php
+session_start();
+require 'dbcon.php';
+
+// Handle candidate submission BEFORE HTML is rendered
+if (isset($_POST['save'])) {
+    $party = $_POST['party'];
+    $position = $_POST['position'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $year_level = $_POST['year_level'];
+    $gender = $_POST['gender'];
+
+    $image = $_FILES['image'];
+    $image_name = addslashes($image['name']);
+    $tmp_name = $image['tmp_name'];
+    $location = "upload/" . $image_name;
+
+    if (move_uploaded_file($tmp_name, $location)) {
+        $stmt = $conn->prepare("INSERT INTO candidate(position, party, firstname, lastname, year_level, gender, img) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssssss", $position, $party, $firstname, $lastname, $year_level, $gender, $location);
+
+        if ($stmt->execute()) {
+            $_SESSION['success'] = "Candidate added successfully.";
+        } else {
+            $_SESSION['error'] = "Failed to add candidate: " . $stmt->error;
+        }
+        $stmt->close();
+    } else {
+        $_SESSION['error'] = "Image upload failed.";
+    }
+
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit();
+}
+?>
+
+<?php include('session.php'); ?>
+<?php include('head.php'); ?>
 
 <body>
+<div id="wrapper">
 
-    <div id="wrapper">
+    <?php include('side_bar.php'); ?>
 
-        <!-- Navigation -->
-        <?php include ('side_bar.php');?>
-
-        <!-- Page Content -->
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h3 class="page-header">Candidate List</h3>
-					
-                </div>
-				
-				<button class="btn btn-success" data-toggle="modal" data-target="#myModal">Add Candidate</button>
-				<?php include ('add_candidate_modal.php');?>
-                 <a href="candidate_excel.php"><button type="button" style = "margin-right:14px;" id ="print" class = "pull-right btn btn-info"><i class = "fa fa-print"></i>Export to Excel</button></a>
-                
-                <!-- /.col-lg-12 -->
-				
-				
-				<hr/>
-				
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="modal-title" id="myModalLabel">         
-												<div class="panel panel-primary">
-													<div class="panel-heading">
-														Candidate List
-													</div>    
-												</div>
-											</h4>
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                    <thead>
-                                        <tr>
-                                         
-                                            
-                                            <th>Image</th>
-                                            <th>Position</th>
-                                            <th>Party</th>
-                                            <th>Firstname</th>
-                                            <th>Lastname</th>
-                                            <th>Year Level</th>
-                                            <th>Gender</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-									
-                                        <tr>
-										<?php 
-											require 'dbcon.php';
-											$bool = false;
-											$query = $conn->query("SELECT * FROM candidate ORDER BY candidate_id DESC");
-												while($row = $query->fetch_array()){
-													$candidate_id=$row['candidate_id'];
-										?>
-											<td width="50"><img src="<?php echo $row['img']; ?>" width="50" height="50" class="img-rounded"></td>
-											<td><?php echo $row ['position'];?></td>
-                                            <td><?php echo $row ['party'];?></td>
-                                            <td><?php echo $row ['firstname'];?></td>
-                                            <td><?php echo $row ['lastname'];?></td>
-                                            <td><?php echo $row ['year_level'];?></td>
-                                            <td><?php echo $row ['gender'];?></td>
-                                            
-                                            
-                                            <td style="text-align:center">
-											
-												 <a rel="tooltip"  title="Delete" id="<?php echo $candidate_id; ?>" href="#delete_user<?php echo $candidate_id; ?>" data-target="#delete_user<?php echo $candidate_id?>" data-toggle="modal"class="btn btn-danger btn-outline"><i class="fa fa-trash-o"></i> Delete</a>	
-											 <?php include ('delete_candidate_modal.php'); ?>
-												  <a rel="tooltip"  title="Edit" id="<?php echo $row['candidate_id'] ?>" href="#edit_candidate<?php echo $row['candidate_id'] ?>"  data-toggle="modal"class="btn btn-success btn-outline"><i class="fa fa-pencil"></i> Edit</a>	
-												
-											</td>
-														
-											    <?php 
-													
-													require 'edit_candidate_modal.php';
-												?>
-                                        </tr>
-										
-                                       <?php } ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.table-responsive -->
-                            
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-              
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <h3 class="page-header">Candidate List</h3>
             </div>
-            <!-- /.row -->
+
+            <!-- Flash Messages -->
+            <div class="col-md-12">
+                <?php if (isset($_SESSION['success'])): ?>
+                    <div class="alert alert-success"><?= $_SESSION['success']; unset($_SESSION['success']); ?></div>
+                <?php endif; ?>
+
+                <?php if (isset($_SESSION['error'])): ?>
+                    <div class="alert alert-danger"><?= $_SESSION['error']; unset($_SESSION['error']); ?></div>
+                <?php endif; ?>
+            </div>
+
+            <div class="col-md-12">
+                <button class="btn btn-success" data-toggle="modal" data-target="#myModal">Add Candidate</button>
+                <a href="candidate_excel.php" class="btn btn-info pull-right" style="margin-right:14px;">
+                    <i class="fa fa-print"></i> Export to Excel
+                </a>
+            </div>
         </div>
-        <!-- /#page-wrapper -->
 
+        <hr/>
 
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="modal-title">Candidate List</h4>
+            </div>
+            <div class="panel-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr>
+                                <th>Image</th>
+                                <th>Position</th>
+                                <th>Party</th>
+                                <th>Firstname</th>
+                                <th>Lastname</th>
+                                <th>Year Level</th>
+                                <th>Gender</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $query = $conn->query("SELECT * FROM candidate ORDER BY candidate_id DESC");
+                            while ($row = $query->fetch_array()):
+                                $candidate_id = $row['candidate_id'];
+                            ?>
+                            <tr>
+                                <td><img src="<?= $row['img']; ?>" width="50" height="50" class="img-rounded"></td>
+                                <td><?= $row['position']; ?></td>
+                                <td><?= $row['party']; ?></td>
+                                <td><?= $row['firstname']; ?></td>
+                                <td><?= $row['lastname']; ?></td>
+                                <td><?= $row['year_level']; ?></td>
+                                <td><?= $row['gender']; ?></td>
+                                <td style="text-align:center">
+                                    <a href="#delete_user<?= $candidate_id; ?>" data-toggle="modal" class="btn btn-danger btn-outline">
+                                        <i class="fa fa-trash-o"></i> Delete
+                                    </a>
+                                    <?php include('delete_candidate_modal.php'); ?>
 
-    </div>
-    <!-- /#wrapper -->
+                                    <a href="#edit_candidate<?= $candidate_id; ?>" data-toggle="modal" class="btn btn-success btn-outline">
+                                        <i class="fa fa-pencil"></i> Edit
+                                    </a>
+                                    <?php include('edit_candidate_modal.php'); ?>
+                                </td>
+                            </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
 
-    <?php include ('script.php');?>
+    </div> <!-- /#page-wrapper -->
 
+</div> <!-- /#wrapper -->
+
+<?php include('add_candidate_modal.php'); ?>
+<?php include('script.php'); ?>
 </body>
-
 </html>
-
