0208e1fa0403beacac3448f5e1b24b8cebf681c6
jayumaks <jayumaks@gmail.com>
2025-07-21 02:45:27 +0100

    fix student login

--- side_bar.php
+++ side_bar.php
@@ -1,30 +1,38 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start(); // Start session if not already started
+}
+
+require 'admin/dbcon.php';
+
+// Check if voter is logged in
+if (!isset($_SESSION['voters_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$session_id = $_SESSION['voters_id'];
+$query = $conn->query("SELECT * FROM voters WHERE voters_id = '$session_id'") or die(mysqli_error($conn));
+$row = $query->fetch_assoc(); // only one user expected
+?>
+
 <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom:0; background-color:black;">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="index.php" style = "color:white; padding-left:25px;"><i class = "fa fa-home fa-large" > </i>HOME | AAU Online Voting System</a>
+    <div class="navbar-header">
+        <a class="navbar-brand" href="index.php" style="color:white; padding-left:25px;">
+            <i class="fa fa-home fa-large"></i> HOME | AAU Online Voting System
+        </a>
+    </div>
 
-            </div>
-
-
-            <ul class="nav navbar-top-links navbar-right">
-
-               <?php require 'admin/dbcon.php';
-				$query = $conn->query("SELECT * from voters where voters_id ='$session_id'")or die (mysql_error ());
-
-				while ($row = $query->fetch_array()){
-
-
-			 ?>
-
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" style = "color: white">
-					<i>Welcome: <?php echo $user_username = $user_row['firstname']." ".$user_row['lastname'];?></i>
-                    </a>
-                </li>
-            <li class="dropdown">
-                <a href="logout.php"style = "color: white; padding-rignt: 30px;"> <i class = "fa fa-sign-out" style = "color: white"></i>Logout</a>
-            </li>
-
-            </ul>
-			<?php }?>
-        </nav>
-
+    <ul class="nav navbar-top-links navbar-right">
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="color: white">
+                <i>Welcome: <?php echo $row['firstname'] . " " . $row['lastname']; ?></i>
+            </a>
+        </li>
+        <li class="dropdown">
+            <a href="logout.php" style="color: white; padding-right: 30px;">
+                <i class="fa fa-sign-out" style="color: white"></i> Logout
+            </a>
+        </li>
+    </ul>
+</nav>
