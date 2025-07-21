cb97ee86993f9322ee05edae0171682a682c281b
jayumaks <jayumaks@gmail.com>
2025-07-21 02:37:11 +0100

    fix student login

--- vote.php
+++ vote.php
@@ -1,10 +1,15 @@
 <?php
-session_start();
-require_once 'admin/dbcon.php';
+// Enable error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// Redirect if voter is not logged in
+// Start session and check login
+session_start();
+require_once('admin/dbcon.php');
+
 if (!isset($_SESSION['voters_id'])) {
-    header("Location: index.php");
+    header("Location: login.php");
     exit();
 }
 ?>
@@ -12,58 +17,44 @@ if (!isset($_SESSION['voters_id'])) {
 <?php include('head.php'); ?>
 <?php include('side_bar.php'); ?>
 
+<!-- Page content starts here -->
 <body>
-    <div class="container" style="margin-top: 60px;">
-        <form method="POST" action="vote_result.php">
-            <div class="row">
+<form method="POST" action="vote_result.php" style="margin-top: 60px;">
+<div class="container">
+    <!-- Example: President Panel -->
+    <div class="col-lg-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading text-center">PRESIDENT</div>
+            <div class="panel-body">
                 <?php
-                // Define all positions you want to display
-                $positions = [
-                    'President',
-                    'Vice President',
-                    'Treasurer',
-                    'Secretary General',
-                    'Welfare',
-                    'Publicity Secretary'
-                ];
-
-                foreach ($positions as $position) {
-                    $query = $conn->prepare("SELECT * FROM candidate WHERE position = ?");
-                    $query->bind_param("s", $position);
-                    $query->execute();
-                    $result = $query->get_result();
-
-                    if ($result->num_rows > 0): ?>
-                        <div class="col-lg-6">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading text-center">
-                                    <?php echo strtoupper($position); ?>
-                                </div>
-                                <div class="panel-body text-center">
-                                    <?php while ($candidate = $result->fetch_assoc()): ?>
-                                        <div class="candidate-block" style="margin-bottom: 20px;">
-                                            <img src="admin/<?php echo $candidate['img']; ?>" class="img-thumbnail" height="150" width="150"><br>
-                                            <strong><?php echo $candidate['firstname'] . ' ' . $candidate['lastname']; ?></strong><br>
-                                            <small>Gender: <?php echo $candidate['gender']; ?> | Level: <?php echo $candidate['year_level']; ?> | Party: <?php echo $candidate['party']; ?></small><br><br>
-                                            <input type="radio" name="<?php echo strtolower(str_replace(' ', '_', $position)); ?>_id"
-                                                   value="<?php echo $candidate['candidate_id']; ?>" required> Select
-                                        </div>
-                                    <?php endwhile; ?>
-                                </div>
-                            </div>
-                        </div>
-                    <?php
-                    endif;
-                }
+                $query = $conn->query("SELECT * FROM candidate WHERE position = 'President'") or die($conn->error);
+                while ($row = $query->fetch_assoc()):
                 ?>
+                    <div id="position">
+                        <center>
+                            <img src="admin/<?php echo $row['img']; ?>" height="150" width="150" class="img-rounded"><br>
+                            <strong>Names:</strong> <?= $row['firstname'] . " " . $row['lastname']; ?><br>
+                            <strong>Gender:</strong> <?= $row['gender']; ?><br>
+                            <strong>Level:</strong> <?= $row['year_level']; ?><br>
+                            <strong>Party:</strong> <?= $row['party']; ?><br>
+                            <input type="checkbox" name="pres_id" value="<?= $row['candidate_id']; ?>" class="pres"> Give Vote
+                        </center>
+                        <hr>
+                    </div>
+                <?php endwhile; ?>
             </div>
-
-            <div class="row text-center" style="margin-top: 30px;">
-                <button type="submit" class="btn btn-success btn-lg" name="submit">Submit Ballot</button>
-            </div>
-        </form>
+        </div>
     </div>
 
-    <?php include('script.php'); ?>
+    <!-- Repeat similar blocks for VP, Treasurer, etc. -->
+
+    <div class="col-lg-12 text-center">
+        <button class="btn btn-success" type="submit" name="submit">Submit Ballot</button>
+    </div>
+</div>
+</form>
+
+<?php include('script.php'); ?>
+<?php include('footer.php'); ?>
 </body>
 </html>
