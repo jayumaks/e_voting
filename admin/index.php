a8c74b1ef094f497fb26efdd6a1bc57b2b3525da
jayumaks <jayumaks@gmail.com>
2025-07-27 22:51:32 +0100

    add show password

--- admin/index.php
+++ admin/index.php
@@ -8,22 +8,22 @@ require_once 'login_query.php';
 ?>
 
 
-<?php include('head.php'); ?>
+<?php include ('head.php');?>
 
 <body>
-    <?php include('index_banner.php'); ?>
+<?php include ('index_banner.php');?>
     <div class="container">
         <div class="row">
 
-            <center>
-                <i>Login As:</i>
-                <select onchange="page(this.value)">
-                    <option selected disables>System Admin</option>
-                    <option value="../admin2/index.php">System User</option>
-                    <option value="../login.php">Student Voter</option>
-                </select>
+                    <center>
+                        <i>Login As:</i>
+                        <select onchange = "page(this.value)">
+                            <option selected disables>System Admin</option>
+                            <option value = "../admin2/index.php">System User</option>
+                            <option value = "../login.php">Student Voter</option>
+                    </select>
 
-            </center>
+                    </center>
             <div class="col-md-4 col-md-offset-4">
                 <div class="login-panel panel panel-default">
 
@@ -32,57 +32,63 @@ require_once 'login_query.php';
                         <center>Admin Log in</center>
                     </div>
                     <div class="panel-body">
-                        <form role="form" method="post" enctype="multipart/form-data">
-                            <!-- <div class="form-group">
+                        <form role="form" method = "post" enctype = "multipart/form-data">
+                                <!-- <div class="form-group">
                                     <label for = "username" >Login ID</label>
                                         <input class="form-control" placeholder="Enter Login ID" name="login_id" type="text" autofocus>
                                 </div> -->
 
-                            <div class="form-group">
-                                <label for="idno">Student ID:</label>
-                                <input class="form-control" name="idno" type="text" required autofocus placeholder="Enter Student ID">
-                            </div>
+                                <div class="form-group">
+									<label for = "username" >Username</label>
+										<input class="form-control" placeholder="Enter Username" name="username" type="text" autofocus>
+                                </div>
 
-                            <div class="form-group">
-                                <label for="password">Password:</label>
-                                <input class="form-control" name="password" id="password" type="password" required placeholder="Enter Password">
-                            </div>
-
-                            <div class="form-check mb-3">
-                                <input type="checkbox" class="form-check-input" id="showPassword" onclick="togglePassword()">
-                                <label class="form-check-label" for="showPassword">Show Password</label>
-                            </div>
-
-                            <script>
-                                function togglePassword() {
-                                    const pwd = document.getElementById("password");
-                                    pwd.type = pwd.type === "password" ? "text" : "password";
-                                }
-                            </script>
+ <div class="form-group" style="position: relative;">
+    <label for="password">Password</label>
+    <input class="form-control" placeholder="Enter Password" name="password" id="password" type="password" value="">
+    <span toggle="#password" class="fa fa-eye-slash toggle-password" style="position: absolute; right: 15px; top: 38px; cursor: pointer;"></span>
+</div>
 
 
 
-                            <button class="btn btn-lg btn-success btn-block " name="login">Login</a>
 
-                                <?php include('login_query.php'); ?>
+                                <button class="btn btn-lg btn-success btn-block " name = "login">Login</a>
+
+									<?php include ('login_query.php');?>
                         </form>
                     </div>
                 </div>
             </div>
 
+			 </div>
         </div>
     </div>
-    </div>
     <script type="text/javascript">
-        function page(src) {
-            window.location = src;
-        }
-    </script>
+  function page (src) {
+    window.location = src;
+  }
+  </script>
 
-    <?php
-    include('script.php');
-    include('footer.php');
-    ?>
+  <?php
+  include ('script.php');
+  include ('footer.php');
+  ?>
+
+
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.querySelector(".toggle-password");
+    const password = document.querySelector("#password");
+
+    toggle.addEventListener("click", function () {
+      const type = password.getAttribute("type") === "password" ? "text" : "password";
+      password.setAttribute("type", type);
+      this.classList.toggle("fa-eye");
+      this.classList.toggle("fa-eye-slash");
+    });
+  });
+</script>
+
 
 </body>
 
