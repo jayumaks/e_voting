9e29011d85616d5b074900b37f6cd6081ca79913
jayumaks <jayumaks@gmail.com>
2025-08-15 07:16:50 +0100

    modify the user page

--- admin2/candidate.php
+++ admin2/candidate.php
@@ -17,7 +17,7 @@
                 </div>
 
 				<!-- <button class="btn btn-success" data-toggle="modal" data-target="#myModal">Add Candidate</button> -->
-				// <?php include ('add_candidate_modal.php');?>
+				 <?php include ('add_candidate_modal.php');?>
                 <!-- /.col-lg-12 -->
 
 
@@ -70,18 +70,9 @@
                                             <td><?php echo $row ['gender'];?></td>
 
 
-                                            <!-- <td style="text-align:center">
 
-												 <a rel="tooltip"  title="Delete" id="<?php echo $candidate_id; ?>" href="#delete_user<?php echo $candidate_id; ?>" data-target="#delete_user<?php echo $candidate_id?>" data-toggle="modal"class="btn btn-danger btn-outline"><i class="fa fa-trash-o"></i> Delete</a>
-											 <?php include ('delete_candidate_modal.php'); ?>
-												  <a rel="tooltip"  title="Edit" id="<?php echo $row['candidate_id'] ?>" href="#edit_candidate<?php echo $row['candidate_id'] ?>"  data-toggle="modal"class="btn btn-success btn-outline"><i class="fa fa-pencil"></i> Edit</a>
 
-											</td> -->
 
-											    <?php
-
-													require 'edit_candidate_modal.php';
-												?>
                                         </tr>
 
                                        <?php } ?>
--- admin2/current_students.php
+++ admin2/current_students.php
@@ -11,56 +11,56 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h3 class="page-header">Current Collage Students</h3>
-				<a href="download.php" class="btn btn-success btn-outline"><i class="glyphicon glyphicon-save"></i> Import Students Data</a>
-                <a href="add_student_id.php" class="btn btn-success btn-outline"><i class="glyphicon glyphicon-save"></i> Add Student ID</a>	
-               
+                    <h3 class="page-header">Current Students</h3>
+				<!-- <a href="download.php" class="btn btn-success btn-outline"><i class="glyphicon glyphicon-save"></i> Import Students Data</a>
+                <a href="add_student_id.php" class="btn btn-success btn-outline"><i class="glyphicon glyphicon-save"></i> Add Student ID</a> -->
+
 				<hr/>
-                
-                
-                
-				
+
+
+
+
                     <div class="panel panel-default">
-                        
+
                         <!-- /.panel-heading -->
                         <div class="panel-body">
                             <div class="table-responsive">
                                 <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                                     <thead>
                                         <tr>
-                                         
+
                                             <th>Student ID</th>
                                             <th>Names</th>
                                             <th>Year Registered</th>
-                                            
+
                                         </tr>
                                     </thead>
                                     <tbody>
-										<?php 
+										<?php
 											require 'dbcon.php';
-											
+
 											$query = $conn->query("SELECT * FROM ids ORDER BY id_number DESC");
 											while($row1 = $query->fetch_array()){
 											$voters_id=$row1['id_number'];
 										?>
-                                      
+
 											<tr>
 												<td><?php echo $row1 ['id_number'];?></td>
 												<td><?php echo $row1 ['names'];?></td>
-												<td><?php echo $row1 ['started'];?></td>		
+												<td><?php echo $row1 ['started'];?></td>
 											</tr>
-										
+
                                        <?php } ?>
                                     </tbody>
                                 </table>
                             </div>
                             <!-- /.table-responsive -->
-                            
+
                         </div>
                         <!-- /.panel-body -->
                     </div>
                     <!-- /.panel -->
-              
+
             </div>
             <!-- /.row -->
         </div>
